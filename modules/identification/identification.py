5270ffba8d1abf840941d3f0a5f28bde5e878d41
Pronoy Mandal <lukex9442@gmail.com>
2023-02-11 01:15:45 +0530

    Use proper paths for local assets

--- modules/identification/identification.py
+++ modules/identification/identification.py
@@ -9,10 +9,9 @@ model = load_model('cattle_identification_keras_model.h5')
 # The 'length' or number of images you can put into the array is
 # determined by the first position in the shape tuple, in this case 1.
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
 # Replace this with the path to your image
-image = Image.open(
-    r"G:\PRONOY\Cattle-cloud\assets\Indian_bovine_breeds\Hallikar\Hallikar_4.jpg"
-)
+image = Image.open('../../assets/Indian_bovine_breeds/Hallikar/Hallikar_4.jpg')
 # resize the image to a 224x224 with the same strategy as in TM2:
 # resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
